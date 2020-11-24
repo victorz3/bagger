7afe730b7d21804c710cd587a2917fcee0f62155
Victor <victor.z@ciencias.unam.mx>
2020-11-24 17:31:33 -0600

    Adds some more rules about packing large items

--- bagger.pl
+++ bagger.pl
@@ -34,22 +34,45 @@ size(chips, medium).
 % Whether item is frozen.
 frozen(ice_cream).
 
+% Auxiliary predicate to delete one element from a list.
+delete_one(_, [], []).
+delete_one(X, [A|AS], L2) :- X == A, L2 = AS, !.
+delete_one(X, [A|AS], L2) :- delete_one(X, AS, L3), L2 = [A|L3].
+
 % Defining orders.
 order(L) :- forall(member(X, L), item(X)).
+
+% Auxiliary predicate to put an item in a bag.
+put_in_bag(B, I, L1, L2) :- delete_one(B, L1, L3), delete_one(I, L3, L4), L2 = [[I|B]|L4].
 
 /* Packing an order means doing the four steps in the text.
  * We'll say that L1 (list of items) packs in L2 (list of bags with items)
  * if this predicate is true. */
-pack(L1, B) :- order(L1), check_order(L1, L2), pack_large_items(L2, L3),
-	       pack_medium_items(L3, L4), pack_small_items(L4, B).
+pack(L1, B) :- order(L1), check_order(L1, L2), pack_large_items(L2, B).
 
 
 % Step check-order.
 
 % Rule B1.
 check_order(L1, L2) :- member(chips, L1), not(member(pepsi, L1)),
-		       L2 = [pepsi | L1].
+		       L2 = [pepsi | L1], !.
+% Rule B2.
+check_order(L1, L1).
 
+% Step pack-large-items
 
+% Checks whether there are large items on the list.
+large_items([X | _]) :- size(X, large), !.
+large_items([_ | XS]) :- large_items(XS).
 
+% Escape clause.
+pack_large_items(L, L) :- not(large_items(L)), !.
 
+% Rule B3.
+pack_large_items(L1, L2) :- size(X, large), type(X, bottle), member(X, L1), member(L, L1), is_list(L), length(L, K), K < 6, put_in_bag(L, X, L1, L3), pack_large_items(L3, L2), !.
+
+% Rule B4.
+pack_large_items(L1, L2) :- size(X, large), member(X, L1), member(L, L1), is_list(L), length(L, K), K < 6, put_in_bag(L, X, L1, L3), pack_large_items(L3, L2), !. 
+
+% Rule B5.
+pack_large_items(L1, L2) :- size(X, large), member(X, L1), L3 = [[]|L1], pack_large_items(L3, L2),!.
